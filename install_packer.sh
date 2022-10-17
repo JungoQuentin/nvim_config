149daca9822e829f4e648f27e8cd84abf366b1dc
qjungo <qjungo@pop-os.localdomain>
2022-10-17 12:48:25 +0200

    improved

---
+++ install_packer.sh
@@ -0,0 +1,7 @@
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
+# sudo apt remove neovim -y
+# sudo add-apt-repository ppa:neovim-ppa/stable
+# sudo apt-get update
+# sudo apt-get install neovim
--- lua/qjungo/packer.lua
+++ lua/qjungo/packer.lua
@@ -4,10 +4,14 @@ return require('packer').startup(function(use)
 	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0'}
 	use {'neovim/nvim-lspconfig'}					--TODO : config more options
 	use {'nvim-lua/plenary.nvim'}
-	use {'nvim-treesitter/nvim-treesitter.nvim', run = 'TSUpdate'}
+	    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+	
 	use {'mbbill/undotree'}
 	use {'42Paris/42header'}
 	-- colorsheme
-	--use {'folke/tokyonight.nvim'}
 	use {'gruvbox-community/gruvbox'}
+	use {'folke/tokyonight.nvim'}
 end)
