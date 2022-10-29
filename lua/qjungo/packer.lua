3f1644b5cf768dd7f3f687295d62f2c6b3116fc4
Quentin Jungo <quentin.jungo@gmail.com>
2022-10-29 18:58:56 +0200

    VimBeGood

--- lua/qjungo/packer.lua
+++ lua/qjungo/packer.lua
@@ -3,6 +3,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0'}
 	use {'neovim/nvim-lspconfig'}					--TODO : config more options
+	use {'ThePrimeagen/vim-be-good'}					--TODO : config more options
 	use {'nvim-lua/plenary.nvim'}
 	    use {
         'nvim-treesitter/nvim-treesitter',
