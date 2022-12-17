82338b9f2a727b6a61aab62bc882c61c10920454
Quentin <quentin.jungo@gmail.com>
2022-12-17 01:10:59 +0100

    clean && add tree sitter

--- init.lua
+++ init.lua
@@ -1,14 +1,13 @@
 require("qjungo")
 
+vim.opt.syntax = "on"
+
 -- Permet d'ouvrir des config dans les projets !!!
 -- disable built-in local config file support
 vim.opt.exrc = false
 require("exrc").setup({
   files = {
-    --".nvimrc.lua",
-    --".nvimrc",
     ".exrc.lua",
-    --".exrc",
   },
 })
 
@@ -20,8 +19,5 @@ vim.g.mail42 = "qjungo@student.42lausanne.ch"
 vim.opt.undofile = true
 vim.g.vim_home_path = "~/nvim"
 vim.g.undodir = string.format("%s/undo", vim.g.vim_home_path)
-vim.opt.swapfile = false	--evite les swap (tjs le bordel)
+vim.opt.swapfile = false
 vim.opt.backup = false
-
-vim.cmd("syntax on")
---vim.cmd("Copilot disable <CR>")
--- lua/qjungo/packer.lua
+++ lua/qjungo/packer.lua
@@ -1,20 +1,18 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use {'wbthomason/packer.nvim'}		-- Pour pas qu'il se supprime
-	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0'}
-	use {'neovim/nvim-lspconfig'}					--TODO : config more options
-	use {'ThePrimeagen/vim-be-good'}					--TODO : config more options
-	-- use {'nvim-lua/plenary.nvim'}
-	--     use {
-    --     'nvim-treesitter/nvim-treesitter',
-    --     run = ':TSUpdate'
-    -- }
+	use {'wbthomason/packer.nvim'}
+	use {'nvim-telescope/telescope.nvim', tag = '0.1.0'}
+	use {"nvim-lua/plenary.nvim"} -- required by telescope
 	use {'ixru/nvim-markdown'}			-- pour le markdown
 	use {'MunifTanjim/exrc.nvim'}		-- pour des vim rc dans tes projets
+	use {'neovim/nvim-lspconfig'}					--TODO : config more options
+	use {'nvim-treesitter/nvim-treesitter'}
+	--use {'nvim-treesitter/nvim-treesitter-textobjects'}
 	use {'habamax/vim-godot'}
 	use {'mbbill/undotree'}
 	use {'42Paris/42header'}
+	use {'ThePrimeagen/vim-be-good'}					--TODO : config more options
 	-- colorsheme
 	use {'gruvbox-community/gruvbox'}
 	use {'folke/tokyonight.nvim'}
