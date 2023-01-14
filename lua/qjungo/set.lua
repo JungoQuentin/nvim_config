21ed28e2434c9c25097d47e3c1f412965e72152c
Quentin <quentin.jungo@gmail.com>
2023-01-14 10:10:38 +0100

    null-ls config removed

--- lua/qjungo/set.lua
+++ lua/qjungo/set.lua
@@ -42,37 +42,39 @@ end
 
 local null_ls = require("null-ls")
 local eslint = require("eslint")
+local prettier = require("prettier")
 
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
 
-null_ls.setup({
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-
-      -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-      vim.api.nvim_create_autocmd(event, {
-        buffer = bufnr,
-        group = group,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
-        end,
-        desc = "[lsp] format on save",
-      })
-    end
-
-    if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-    end
-  end,
-})
+null_ls.setup()
+-- local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+-- local event = "BufWritePre" -- or "BufWritePost"
+-- local async = event == "BufWritePost"
+-- null_ls.setup({
+--   on_attach = function(client, bufnr)
+--     if client.supports_method("textDocument/formatting") then
+--       vim.keymap.set("n", "<Leader>l", function()
+--         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+--       end, { buffer = bufnr, desc = "[lsp] format" })
+-- 
+--       -- format on save
+--       vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+--       vim.api.nvim_create_autocmd(event, {
+--         buffer = bufnr,
+--         group = group,
+--         callback = function()
+--           vim.lsp.buf.format({ bufnr = bufnr, async = async })
+--         end,
+--         desc = "[lsp] format on save",
+--       })
+--     end
+-- 
+--     if client.supports_method("textDocument/rangeFormatting") then
+--       vim.keymap.set("x", "<Leader>l", function()
+--         vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+--       end, { buffer = bufnr, desc = "[lsp] format" })
+--     end
+--   end,
+-- })
 
 eslint.setup({
   bin = 'eslint', -- or `eslint_d`
@@ -94,8 +96,6 @@ eslint.setup({
   },
 })
 
-local prettier = require("prettier")
-
 prettier.setup({
   bin = 'prettier', -- or `'prettierd'` (v0.22+)
   filetypes = {
@@ -111,5 +111,6 @@ prettier.setup({
     "typescript",
     "typescriptreact",
     "yaml",
+	"svelte",
   },
 })
