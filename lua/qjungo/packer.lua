d38382b71e56da2ba6c666d4c5ca588b1afbc8dc
Quentin <quentin.jungo@gmail.com>
2023-01-14 09:45:41 +0100

    JS lsp added

--- lua/qjungo/packer.lua
+++ lua/qjungo/packer.lua
@@ -8,11 +8,17 @@ return require('packer').startup(function(use)
 	use {'MunifTanjim/exrc.nvim'}		-- pour des vim rc dans tes projets
 	use {'neovim/nvim-lspconfig'}					--TODO : config more options
 	use {'nvim-treesitter/nvim-treesitter'}
-	--use {'nvim-treesitter/nvim-treesitter-textobjects'}
 	use {'habamax/vim-godot'}
 	use {'mbbill/undotree'}
 	use {'42Paris/42header'}
 	use {'ThePrimeagen/vim-be-good'}					--TODO : config more options
+
+	-- For JS project
+	use {'jose-elias-alvarez/null-ls.nvim'}
+	use {'MunifTanjim/eslint.nvim'}
+	use {'MunifTanjim/prettier.nvim'}
+	use {'evanleck/vim-svelte'} -- for svelte
+
 	-- colorsheme
 	use {'gruvbox-community/gruvbox'}
 	use {'folke/tokyonight.nvim'}
--- lua/qjungo/set.lua
+++ lua/qjungo/set.lua
@@ -36,3 +36,80 @@ end
 --Folding with treesitter
 --vim.opt.foldmethod = "syntax"
 --vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+
+-- Default config (TODO improve ?)
+
+local null_ls = require("null-ls")
+local eslint = require("eslint")
+
+local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local event = "BufWritePre" -- or "BufWritePost"
+local async = event == "BufWritePost"
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      -- format on save
+      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+      vim.api.nvim_create_autocmd(event, {
+        buffer = bufnr,
+        group = group,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+        end,
+        desc = "[lsp] format on save",
+      })
+    end
+
+    if client.supports_method("textDocument/rangeFormatting") then
+      vim.keymap.set("x", "<Leader>f", function()
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+    end
+  end,
+})
+
+eslint.setup({
+  bin = 'eslint', -- or `eslint_d`
+  code_actions = {
+    enable = true,
+    apply_on_save = {
+      enable = true,
+      types = { "directive", "problem", "suggestion", "layout" },
+    },
+    disable_rule_comment = {
+      enable = true,
+      location = "separate_line", -- or `same_line`
+    },
+  },
+  diagnostics = {
+    enable = true,
+    report_unused_disable_directives = false,
+    run_on = "type", -- or `save`
+  },
+})
+
+local prettier = require("prettier")
+
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.22+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+})
